3b1f03189505e1e4b5a7ff3592d4948afd9295d0
lewis <ljfgudgeon@gmail.com>
2019-10-08 11:23:05 +0100

    Final update

--- engine/binance_connection.py
+++ engine/binance_connection.py
@@ -4,13 +4,12 @@ from pandas.io.json import json_normalize
 from engine.get_pairs_universe import basket_universe
 
 #Get pairs universe
-pairs_universe = basket_universe(5)
+pairs_universe = basket_universe(10)
 
 #Retrieve historical data from Binance for the pairs
 for pair in pairs_universe:
 	response = requests.get('https://api.binance.com/api/v1/klines?symbol=' + str(pair) + '&interval=1h')
+	print(response.status_code)
 	if response.status_code != 200:
 		# This means something went wrong.
 		print('Error ' + str(response.status_code) + ' returned from Binance.')
-		
-
