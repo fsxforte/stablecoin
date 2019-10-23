05efb059792baca5ce3614fa8078be82cbe19762
lewis <ljfgudgeon@gmail.com>
2019-10-23 10:56:20 +0100

    Resuffled code

--- engine/binance_chain.py
+++ engine/binance_chain.py
@@ -1,7 +1,10 @@
 import requests
 import pandas as pd
 import datetime as dt
+from ratelimit import limits
+import pickle
 
+from engine.generic_functions import pairwise, generate_time_pairs
 
 def get_tokens():
 	"""
@@ -32,7 +35,9 @@ def get_markets():
 		print('Not found.')
 	return df
 
-def get_klines(frequency: str, startTime: dt.datetime, endTime: dt.datetime):
+
+@limits(calls = 10, period = 1)
+def get_all_1m_klines():
 	"""
 	Function to retrieve historical price data from the Binance chain. 
 	:param frequency: specify the frequency of the data. Allowed values from:
@@ -40,32 +45,75 @@ def get_klines(frequency: str, startTime: dt.datetime, endTime: dt.datetime):
 	:param startTime: set the start time of the period to retrieve the data from
 	:param endTime: set the end time of the period to retrieve the data from
 	"""
+
+	#Make exlusive timestamp categories
+	timestamp_list = list(pairwise(generate_time_pairs(dt.datetime(2019,1,1), dt.datetime(2019,10,22))))
+	ranges = []
+	for window in timestamp_list:
+		couple = (window[0]*1000+1, window[1] * 1000)
+		ranges.append(couple)
+
 	#Get list of symbols
 	df = get_markets()
 	base_asset_symbols = list(df['base_asset_symbol'])
+
 	#Select pairs where quote symbol is BNB
 	base_asset_symbols_excluding_BNB = []
 	df_master=pd.DataFrame()
 	for item in base_asset_symbols:
 		if item!='BNB':
 			base_asset_symbols_excluding_BNB.append(str(item)+'_BNB')
-	startTime=int(startTime.strftime('%s'))*1000
-	endTime=int(endTime.strftime('%s'))*1000
-	for symbol in base_asset_symbols_excluding_BNB:
-		response = requests.get('https://dex.binance.org/api/v1/klines?symbol=' + str(symbol) + '&interval=' + str(frequency) + '&startTime=' + str(startTime)+'&endTime=' +str(endTime) +'&limit=1000')
-		if response.status_code == 200:
-			df = pd.DataFrame.from_records(response.json(), columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades'])
-			df['symbol']=str(symbol)
-			df['open_time']=pd.to_numeric(df['open_time'])
-			df['open']=pd.to_numeric(df['open'])
-			df['high']=pd.to_numeric(df['high'])
-			df['low']=pd.to_numeric(df['low'])
-			df['close']=pd.to_numeric(df['close'])
-			df['volume']=pd.to_numeric(df['volume'])
-			df['close_time']=pd.to_numeric(df['close_time'])
-			df['quote_asset_volume']=pd.to_numeric(df['quote_asset_volume'])
-			df['number_of_trades']=pd.to_numeric(df['number_of_trades'])
-			df_master=df_master.append(df)
-		else:
-			print('Error retrieving kline data.')
-	return df_master
+
+	for symbol in base_asset_symbols_excluding_BNB[:1]:
+		for window in ranges:
+			response = requests.get('https://dex.binance.org/api/v1/klines?symbol=' + str(symbol) + '&interval=1m' + '&startTime=' + str(window[0])+'&endTime=' +str(window[1]) +'&limit=1000')
+			if response.status_code != 200:
+				print('Error code ' + str(response.status_code) + 'received')
+			elif response.status_code == 200:
+				df = pd.DataFrame.from_records(response.json(), columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades'])
+				df['symbol']=str(symbol)
+				df['open_time']=pd.to_numeric(df['open_time'])
+				df['open']=pd.to_numeric(df['open'])
+				df['high']=pd.to_numeric(df['high'])
+				df['low']=pd.to_numeric(df['low'])
+				df['close']=pd.to_numeric(df['close'])
+				df['volume']=pd.to_numeric(df['volume'])
+				df['close_time']=pd.to_numeric(df['close_time'])
+				df['quote_asset_volume']=pd.to_numeric(df['quote_asset_volume'])
+				df['number_of_trades']=pd.to_numeric(df['number_of_trades'])
+				df_master=df_master.append(df)
+				print(df_master)
+
+	pickle_out = open("./data/kline_data.pkl","wb")
+	pickle.dump(df_master, pickle_out)
+	pickle_out.close()
+
+
+# def dataset_extractor():
+# 	"""
+# 	Extracts all 1m kline data from the Binance API and stores it as a pickle file.
+# 	"""
+# 	df_master = pd.DataFrame()
+# 	#Retrieve historical data from Binance for the pairs
+# 	for pair in pairs_universe:
+# 		for window in ranges:
+# 			response = requests.get('https://api.binance.com/api/v1/klines?symbol=' + str(pair) + '&interval=' + str(frequency) + '&startTime='+str(window[0])+'&endTime='+str(window[1]))
+# 			print(response.status_code)
+# 			if response.status_code != 200:
+# 				# This means something went wrong.
+# 				print('Error ' + str(response.status_code) + ' returned from Binance.')
+# 			else:
+# 				df = pd.DataFrame.from_records(response.json(), columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
+# 				df['pair']=pair
+# 				df['Open time']=df['Open time'].astype(float)
+# 				df['Open'] = df['Open'].astype(float)
+# 				df['High'] = df['High'].astype(float)
+# 				df['Low'] = df['Low'].astype(float)
+# 				df['Close'] = df['Close'].astype(float)
+# 				df['Volume'] = df['Volume'].astype(float)
+# 				df['Close time'] = df['Quote asset volume'].astype(float)
+# 				df['Number of trades'] = df['Taker buy base asset volume'].astype(float)
+# 				df['Taker buy quote asset volume'] = df['Taker buy quote asset volume'].astype(float)
+# 				df['Ignore'] = df['Ignore'].astype(float)
+# 				df_master = df_master.append(df, ignore_index=True)
+# 	df_master.to_pickle("./kline_data.pkl")
